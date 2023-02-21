9e8be70ef2cbfdb91345edcd92a70afd6ab18f4f
SALHI Anas <anas.salhi6@gmail.com>
2023-02-21 14:54:56 +0100

    Structure of promotion

---
+++ src/main/java/com/dosi/promotion/controllers/CandidatController.java
@@ -0,0 +1,7 @@
+package com.dosi.promotion.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CandidatController {
+}
---
+++ src/main/java/com/dosi/promotion/services/CandidatService.java
@@ -0,0 +1,4 @@
+package com.dosi.promotion.services;
+
+public class CandidatService {
+}

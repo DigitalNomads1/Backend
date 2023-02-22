304c42efbc6e4989703014aafda185ddb79512c6
Manal Abdouh <manalabdouh1@gmail.com>
2023-02-22 16:49:48 +0100

    Adding etudiant

---
+++ src/main/java/com/dosi/controllers/EtudiantController.java
@@ -0,0 +1,18 @@
+
+package com.dosi.controllers;
+
+import com.dosi.entities.Etudiant;
+import com.dosi.services.EtudiantService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/etudiant")
+public class EtudiantController extends BaseController<Etudiant,String>  {
+    public EtudiantController(EtudiantService service) {
+        super(service);
+    }
+
+
+}
+

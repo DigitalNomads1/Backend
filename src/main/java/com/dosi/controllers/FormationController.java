d117ba7787edf811a84adfdb7fe8a62f8bc528db
nora youssefi <nora.youssefi@etu.uae.ac.ma>
2023-02-22 16:19:14 +0100

    commit v3.2

---
+++ src/main/java/com/dosi/controllers/FormationController.java
@@ -0,0 +1,15 @@
+package com.dosi.controllers;
+
+import com.dosi.entities.Formation;
+import com.dosi.services.FormationService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/formations")
+public class FormationController extends BaseController<Formation, String>{
+    public FormationController(FormationService service) {
+        super(service);
+    }
+
+}

f333317b4756161006e838dcf51254947f97100c
HajarElansary <hajarbe2016@gmail.com>
2023-02-23 11:27:57 +0100

    formation ue v1.4

--- src/main/java/com/dosi/repositories/UniteEnseignementRepository.java
+++ src/main/java/com/dosi/repositories/UniteEnseignementRepository.java
@@ -1,8 +1,13 @@
 package com.dosi.repositories;
 
+import com.dosi.entities.Formation;
 import com.dosi.entities.UniteEnseignement;
 import com.dosi.entities.UniteEnseignementId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UniteEnseignementRepository extends JpaRepository<UniteEnseignement, UniteEnseignementId> {
+
+    List<UniteEnseignement> findByCodeFormation(Formation formation);
 }
--- src/main/java/com/dosi/services/FormationService.java
+++ src/main/java/com/dosi/services/FormationService.java
@@ -1,12 +1,27 @@
 package com.dosi.services;
 
 import com.dosi.entities.Formation;
+import com.dosi.entities.UniteEnseignement;
 import com.dosi.repositories.FormationRepository;
+
+import com.dosi.repositories.UniteEnseignementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FormationService extends BaseService<Formation, String>{
     public FormationService(FormationRepository formationRepository){
+
         super(formationRepository);
+
     }
+
+    @Autowired
+    UniteEnseignementRepository uniteEnseignementRepository;
+    public List<UniteEnseignement> findUEList(String id){
+        Formation formation = repository.findById(id).get();
+        return uniteEnseignementRepository.findByCodeFormation(formation) ;  }
+
 }

3dac763d276e83afba04f49b5826367a9d85c428
SALHI Anas <anas.salhi6@gmail.com>
2023-02-23 16:20:33 +0100

    Fixing stuff

--- src/main/java/com/dosi/entities/Enseignant.java
+++ src/main/java/com/dosi/entities/Enseignant.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter

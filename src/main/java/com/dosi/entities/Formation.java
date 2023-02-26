a8cfec3082319ea229fa18458b3c3c2d380d867f
khalid tounoussi <khalid.tounoussi@etudiant.univ-brest.fr>
2023-02-26 21:52:26 +0100

    excpetion handling

--- src/main/java/com/dosi/entities/Formation.java
+++ src/main/java/com/dosi/entities/Formation.java
@@ -3,11 +3,14 @@ package com.dosi.entities;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -42,13 +45,25 @@ public class Formation implements Identifiable<String>{
     private String doubleDiplome;
 
     @Column(name = "DEBUT_ACCREDITATION")
+    //@NotNull(message = "debutAccreditation est Requis!")
+    //@DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate debutAccreditation;
 
     @Column(name = "FIN_ACCREDITATION")
+   // @NotNull(message = "finAccreditation est Requis!")
+   // @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate finAccreditation;
 /*
     @OneToMany(mappedBy = "codeFormation", fetch = FetchType.EAGER)
     @JsonBackReference
     private List<UniteEnseignement> listUE;*/
+/*
+    @AssertTrue(message = "La date de début d'accréditation doit être antérieure à la date de fin d'accréditation")
+    public boolean isDebutAccreditationBeforeFinAccreditation() {
+        if (debutAccreditation == null || finAccreditation == null) {
+            return true; // skip validation if either date is null
+        }
+        return debutAccreditation.isBefore(finAccreditation);
+    }*/
 
 }

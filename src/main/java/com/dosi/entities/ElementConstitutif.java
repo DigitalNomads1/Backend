acbd335937d14c6cf7e707f2aa5c1f645a8f45a9
HajarElansary <hajarbe2016@gmail.com>
2023-02-27 17:45:32 +0100

    ENTITY VERIFICATION

--- src/main/java/com/dosi/entities/ElementConstitutif.java
+++ src/main/java/com/dosi/entities/ElementConstitutif.java
@@ -2,6 +2,8 @@ package com.dosi.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -12,10 +14,12 @@ import lombok.*;
 @AllArgsConstructor
 @Table(name = "ELEMENT_CONSTITUTIF")
 public class ElementConstitutif implements Identifiable<ElementConstitutifId>{
+    @NotNull(message = "id est Requis!")
+    @Valid
     @EmbeddedId
     private ElementConstitutifId id;
 
-    @Column(name = "DESIGNATION", nullable = false, length = 64)
+    @Column(name = "DESIGNATION", length = 64)
     private String designation;
 
     @Column(name = "DESCRIPTION", length = 240)
@@ -31,15 +35,17 @@ public class ElementConstitutif implements Identifiable<ElementConstitutifId>{
     private Short nbhTp;
 
     @MapsId("codeUE")
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "CODE_UE", referencedColumnName = "CODE_UE")
-    @JoinColumn(name = "CODE_FORMATION", referencedColumnName = "CODE_FORMATION")
+    @ManyToOne(fetch = FetchType.LAZY , optional = false)
+    @JoinColumn(name = "CODE_UE", referencedColumnName = "CODE_UE", nullable = false)
+    @JoinColumn(name = "CODE_FORMATION", referencedColumnName = "CODE_FORMATION", nullable = false)
     @JsonIgnoreProperties({"hibernateLazyInitializer"})
+    @NotNull(message = "codeUE est Requis!")
     private UniteEnseignement codeUE;
 
-    @MapsId("no_enseignant")
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "NO_ENSEIGNANT", referencedColumnName = "NO_ENSEIGNANT")
+    @NotNull(message = "noEnseignant est Requis!")
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "NO_ENSEIGNANT", nullable = false)
     @JsonIgnoreProperties({"hibernateLazyInitializer"})
     private Enseignant no_enseignant;
+
 }

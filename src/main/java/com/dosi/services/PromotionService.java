902360272c4626c3c262ef5bb5efc2268455a7fa
SALHI Anas <anas.salhi6@gmail.com>
2023-02-27 17:17:43 +0100

    Fixing delete promotion.

--- src/main/java/com/dosi/services/PromotionService.java
+++ src/main/java/com/dosi/services/PromotionService.java
@@ -4,12 +4,20 @@ import com.dosi.entities.Promotion;
 import com.dosi.entities.PromotionId;
 import com.dosi.repositories.FormationRepository;
 import com.dosi.repositories.PromotionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PromotionService extends BaseService<Promotion, PromotionId>{
+    @Autowired
+    PromotionRepository repository;
     public PromotionService(PromotionRepository promotionRepository){
         super(promotionRepository);
+    }
+
+    public void deleteByEmbeddedId(String annee,String formation)
+    {
+        repository.deleteByEmbeddedId(annee,formation);
     }
 
 }

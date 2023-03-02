cd59665c67965a65d3e9048babed2f404527e845
HajarElansary <hajarbe2016@gmail.com>
2023-03-02 11:03:37 +0100

    test EC

--- src/test/java/com/dosi/services/ECServiceTest.java
+++ src/test/java/com/dosi/services/ECServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -115,7 +116,7 @@ public class ECServiceTest {
     }
 
     @Test
-    public void testFindById(){
+    public void testFindById() {
         Formation formation = Formation.builder()
                 .id("FORMATION_TEST")
                 .diplome("TEST")
@@ -126,7 +127,233 @@ public class ECServiceTest {
                 .id(1L)
                 .prenom("John")
                 .nom("Doe")
-                .build();}
+                .build();
 
+
+        UniteEnseignementId uniteEnseignementId = UniteEnseignementId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .build();
+
+
+        UniteEnseignement uniteEnseignement = UniteEnseignement.builder()
+                .id(uniteEnseignementId)
+                .codeFormation(formation)
+                .noEnseignant(enseignant)
+                .designation("designation test 1")
+                .semestre("1")
+                .description("description test 1")
+                .nbhCm(20L)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+
+        ElementConstitutifId elementConstitutifId = ElementConstitutifId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .codeEc("EC_TEST")
+                .build();
+
+        ElementConstitutif elementConstitutif = ElementConstitutif.builder()
+                .id(elementConstitutifId)
+                .codeUE(uniteEnseignement)
+                .no_enseignant(enseignant)
+                .nbhCm((short)20)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+        when(elementConstitutifRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(elementConstitutif));
+
+        ElementConstitutif result = ecService.read(elementConstitutifId);
+
+        assertEquals("EC_TEST",result.getId().getCodeEc());
+        assertEquals("UE_TEST",result.getCodeUE().getId().getCodeUe());
+        assertEquals("FORMATION_TEST",result.getCodeUE().getId().getCodeFormation());
+    }
+
+    @Test
+    public void TestCreateEC(){
+        Formation formation = Formation.builder()
+                .id("FORMATION_TEST")
+                .diplome("TEST")
+                .n0Annee(1)
+                .build();
+
+        Enseignant enseignant = Enseignant.builder()
+                .id(1L)
+                .prenom("John")
+                .nom("Doe")
+                .build();
+
+
+        UniteEnseignementId uniteEnseignementId = UniteEnseignementId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .build();
+
+
+        UniteEnseignement uniteEnseignement = UniteEnseignement.builder()
+                .id(uniteEnseignementId)
+                .codeFormation(formation)
+                .noEnseignant(enseignant)
+                .designation("designation test 1")
+                .semestre("1")
+                .description("description test 1")
+                .nbhCm(20L)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+
+        ElementConstitutifId elementConstitutifId = ElementConstitutifId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .codeEc("EC_TEST")
+                .build();
+
+        ElementConstitutif elementConstitutif = ElementConstitutif.builder()
+                .id(elementConstitutifId)
+                .codeUE(uniteEnseignement)
+                .no_enseignant(enseignant)
+                .nbhCm((short)20)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+
+        when(elementConstitutifRepository.save(any(ElementConstitutif.class))).thenReturn(elementConstitutif);
+
+        ElementConstitutif result = ecService.create(elementConstitutif);
+
+        assertEquals("EC_TEST",result.getId().getCodeEc());
+        assertEquals("UE_TEST",result.getCodeUE().getId().getCodeUe());
+        assertEquals("FORMATION_TEST",result.getCodeUE().getId().getCodeFormation());
+        assertEquals("John",result.getNo_enseignant().getPrenom());
+        assertEquals("Doe",result.getNo_enseignant().getNom());
+    }
+
+    @Test
+    public void testUpdateEC(){
+        Formation formation = Formation.builder()
+                .id("FORMATION_TEST")
+                .diplome("TEST")
+                .n0Annee(1)
+                .build();
+
+        Enseignant enseignant = Enseignant.builder()
+                .id(1L)
+                .prenom("John")
+                .nom("Doe")
+                .build();
+
+
+        UniteEnseignementId uniteEnseignementId = UniteEnseignementId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .build();
+
+
+        UniteEnseignement uniteEnseignement = UniteEnseignement.builder()
+                .id(uniteEnseignementId)
+                .codeFormation(formation)
+                .noEnseignant(enseignant)
+                .designation("designation test 1")
+                .semestre("1")
+                .description("description test 1")
+                .nbhCm(20L)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+
+        ElementConstitutifId elementConstitutifId = ElementConstitutifId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .codeEc("EC_TEST")
+                .build();
+
+        ElementConstitutif elementConstitutif = ElementConstitutif.builder()
+                .id(elementConstitutifId)
+                .codeUE(uniteEnseignement)
+                .no_enseignant(enseignant)
+                .nbhCm((short)20)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+
+        when(elementConstitutifRepository.save(any(ElementConstitutif.class))).thenReturn(elementConstitutif);
+
+        ElementConstitutif result = ecService.update(elementConstitutif);
+
+        assertEquals("EC_TEST",result.getId().getCodeEc());
+        assertEquals("UE_TEST",result.getCodeUE().getId().getCodeUe());
+        assertEquals("FORMATION_TEST",result.getCodeUE().getId().getCodeFormation());
+        assertEquals("John",result.getNo_enseignant().getPrenom());
+        assertEquals("Doe",result.getNo_enseignant().getNom());
+        assertEquals((short) 20,result.getNbhCm());
+
+        verify(elementConstitutifRepository, times(1)).save(any(ElementConstitutif.class));
+    }
+
+
+    @Test
+    public void testDeleteEC() {
+        Formation formation = Formation.builder()
+                .id("FORMATION_TEST")
+                .diplome("TEST")
+                .n0Annee(1)
+                .build();
+
+        Enseignant enseignant = Enseignant.builder()
+                .id(1L)
+                .prenom("John")
+                .nom("Doe")
+                .build();
+
+
+        UniteEnseignementId uniteEnseignementId = UniteEnseignementId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .build();
+
+
+        UniteEnseignement uniteEnseignement = UniteEnseignement.builder()
+                .id(uniteEnseignementId)
+                .codeFormation(formation)
+                .noEnseignant(enseignant)
+                .designation("designation test 1")
+                .semestre("1")
+                .description("description test 1")
+                .nbhCm(20L)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+
+        ElementConstitutifId elementConstitutifId = ElementConstitutifId.builder()
+                .codeFormation("FORMATION_TEST")
+                .codeUe("UE_TEST")
+                .codeEc("EC_TEST")
+                .build();
+
+        ElementConstitutif elementConstitutif = ElementConstitutif.builder()
+                .id(elementConstitutifId)
+                .codeUE(uniteEnseignement)
+                .no_enseignant(enseignant)
+                .nbhCm((short) 20)
+                .nbhTd((short) 20)
+                .nbhTp((short) 2)
+                .build();
+
+        when(elementConstitutifRepository.findById(elementConstitutifId))
+                .thenReturn(Optional.of(ElementConstitutif.builder()
+                        .id(elementConstitutifId)
+                        .codeUE(uniteEnseignement)
+                        .no_enseignant(enseignant)
+                        .nbhCm((short) 20)
+                        .nbhTd((short) 20)
+                        .nbhTp((short) 2)
+                        .build()));
+        ecService.delete(elementConstitutifId);
+
+        verify(elementConstitutifRepository, times(1)).deleteById(elementConstitutifId);
+        verify(elementConstitutifRepository, times(1)).deleteById(elementConstitutifId);
+    }
 
 }
